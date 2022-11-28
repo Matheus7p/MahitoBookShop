a501aa7094494d24cbf5fde4d3ac3c65037d640e
Matheus7p <matheus.costa.7p@gmail.com>
2022-11-28 02:39:04 -0300

    Finalizado NavBar Responsiva

--- index.php
+++ index.php
@@ -1,22 +1,63 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
 
-    <title>Login</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="Assets/favicon.ico" type="image/x-icon">
+  <style>
+    <?php
+    include 'style.css';
+    ?>
+  </style>
+
+  <title>Mahito Book Shop</title>
+</head>
+
+<body>
+
+  <nav>
+    <a class="logo" href="/">Mahito <span>Book Shop</span></a>
+    <div class="search-box">
+      <input type="search" class="search-txt" placeholder="Pesquisar">
+      <a href="#" class="search-btn">
+        <img src="Assets/loupe.svg" alt="Lupa" height="20" width="20">
+      </a>
+    </div>
+    <div class="mobile-menu">
+      <div class="line1"></div>
+      <div class="line2"></div>
+      <div class="line3"></div>
+    </div>
+    <ul class="nav-list">
+      <div class="menu-li-icons">
+        <li>
+          <a class="carrinho" href="#"> 
+          <img class="carrinho-img" src="Assets/cart.svg" alt="Carrinho"> Carrinho</a>
+        </li>
+      </div>
+      <div class="menu-li-icons">
+        <li>
+          <a class="conta" href="#"> 
+          <img class="conta-img" src="Assets/person-circle.svg" alt="Conta"> Minha Conta</a>
+        </li>
+      </div>
+
+      <div class="menu-li-icons">
+        <li>
+          <a class="coracao" href="#"> 
+          <img class="coracao-img" src="Assets/heart.svg" alt="Favoritos"> Favoritos</a>
+        </li>
+      </div>
+      
+    </ul>
+
+  </nav>
+  </header>
+  <main></main>
+  <script src="Js/mobile-navbar.js"></script>
 
 
+</body>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    
-  </body>
 </html>
