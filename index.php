6dd2ed9ee2a323be7e6f62f1d479051eca4b4ba4
Matheus7p <matheus.costa.7p@gmail.com>
2022-12-01 01:55:44 -0300

    adicionando menu dropdown

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 <body>
 
-  <nav>
+  <nav class="navSuperior">
     <a class="logo" href="/">Mahito <span>Book Shop</span></a>
     <div class="search-box">
       <input type="search" class="search-txt" placeholder="Pesquisar">
@@ -32,32 +32,81 @@
     <ul class="nav-list">
       <div class="menu-li-icons">
         <li>
-          <a class="carrinho" href="#"> 
-          <img class="carrinho-img" src="Assets/cart.svg" alt="Carrinho"> Carrinho</a>
+          <a class="carrinho" href="#">
+            <img class="carrinho-img" src="Assets/cart.svg" alt="Carrinho"> Carrinho</a>
         </li>
       </div>
       <div class="menu-li-icons">
         <li>
-          <a class="conta" href="#"> 
-          <img class="conta-img" src="Assets/person-circle.svg" alt="Conta"> Minha Conta</a>
+          <a class="conta" href="#">
+            <img class="conta-img" src="Assets/person-circle.svg" alt="Conta"> Minha Conta</a>
         </li>
       </div>
 
       <div class="menu-li-icons">
         <li>
-          <a class="coracao" href="#"> 
-          <img class="coracao-img" src="Assets/heart.svg" alt="Favoritos"> Favoritos</a>
+          <a class="coracao" href="#">
+            <img class="coracao-img" src="Assets/heart.svg" alt="Favoritos"> Favoritos</a>
         </li>
       </div>
-      
+
     </ul>
 
   </nav>
   </header>
-  <main></main>
+  <main>
+
+
+    <nav class="navDropdown">
+      <ul class="dropdown-menu">
+        <li>
+          <a href="#"><b>Livros</b> &#9660;</a>
+
+          <ul class="dropdown-submenu">
+            <li><a href="#"><b>Romance</b></a></li>
+            <li><a href="#"><b>Aventura</b></a></li>
+            <li><a href="#"><b>Didáticos</b></a></li>
+            <li><a href="#"><b>Terror</b></a></li>
+            <li><a href="#"><b>Ficção</b></a></li>
+            <li><a href="#"><b>Crônicas</b></a></li>
+            <li><a href="#"><b>Conto</b></a></li>
+          </ul>
+
+        </li>
+        <li><a href="#"><b>Vendidos</b></a></li>
+        <li>
+          <a href="#"><b>Mangás</b> &#9660;</a>
+
+          <ul class="dropdown-submenu">
+            <li><a href="#"><b>One Piece</b></a></li>
+            <li><a href="#"><b>Naruto</b></a></li>
+            <li><a href="#"><b>Jujutsu Kaisen</b></a></li>
+            <li><a href="#"><b>Kimetsu no Yaiba</b></a></li>
+            <li><a href="#"><b>Dragon Ball</b></a></li>
+            <li><a href="#"><b>Pokémon</b></a></li>
+          </ul>
+
+        </li>
+        <li><a href="#"><b>Geek</b></a></li>
+        <li>
+          <a href="#"><b>Jogos | Brinquedo</b> &#9660;</a>
+
+          <ul class="dropdown-submenu">
+            <li><a href="#"><b>Quebra-Cabeça</b></a></li>
+            <li><a href="#"><b>Dungeons & Dragons</b></a></li>
+            <li><a href="#"><b>Banco Imobiliario</b></a></li>
+            <li><a href="#"><b>Bonecas</b></a></li>
+            <li><a href="#"><b>Carrinhos</b></a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+
+
+  </main>
+
+
   <script src="Js/mobile-navbar.js"></script>
-
-
 </body>
 
 </html>
